912d8f71634d945f04d9dbb4712489642d96ef8e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-02-03 16:16:52 -0500

    Add new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
     render :index
   end
+
+  def new
+    @product = Product.new
+    render :new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "products#index"
+
+  resources :products
 end
